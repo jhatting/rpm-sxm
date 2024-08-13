cfd42e8f28de1048dbdced37c6d374789eacbc65
jhatting <jarrodhatting@icloud.com>
2024-08-13 09:48:16 -0500

    Website built

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -4,24 +4,16 @@ import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
 
 export const SOCIALS = [
-  {
-    name: "Github",
-    url: "https://github.com/thevrus",
-    icon: IconGithub,
-  },
+  
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
+    url: "https://www.linkedin.com",
     icon: IconLinkedin,
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
+    url: "https://twitter.com/",
     icon: IconTwitter,
   },
-  {
-    name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
-    icon: IconDribbble,
-  },
+  
 ];
