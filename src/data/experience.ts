362d0ac215b2dbc05ac3c31e412a1d6fdb3df6bd
jhatting <jarrodhatting@icloud.com>
2024-08-13 10:34:01 -0500

    Dollar changes

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,7 +4,7 @@ export const EXPERIENCE: ExperienceProp[] = [
   {
     dates: "Mandatory Services",
     title: "Transform Your Property into a Renter's Dream ",
-    company: "with Royal Property Management's Elite Services!",
+    company: "$50 per hour",
     companyUrl: "https://rpm-sxm.s3.us-east-2.amazonaws.com/Mandatory.pdf",
     description:
       "At Royal Property Management, our mandatory services are designed to ensure your property is in top condition and ready to impress potential renters. We provide comprehensive fumigation, furniture treatment, mold removal, and deep cleaning for every corner of your property, including cupboards, drawers, kitchens, bathrooms, air conditioners, and bed linens. We also conduct a thorough inspection of the entire property to ensure everything is functioning properly and meets our high standards. In addition, we offer expert advice on how to enhance your rental unit's appeal to attract more business.",
@@ -22,7 +22,7 @@ export const EXPERIENCE: ExperienceProp[] = [
   {
     dates: "Maintenance Services",
     title: "Year-Round Excellence:",
-    company: "Keep Your Property in Peak Condition",
+    company: "$60 per hour",
     companyUrl: "https://rpm-sxm.s3.us-east-2.amazonaws.com/Maintenance.pdf",
     description:
       "Our regular maintenance services are key to keeping your property in excellent shape throughout the year. We offer weekly checks on essential systems such as air conditioning, internet, TV, cameras, and appliances like refrigerators, laundry machines, and kitchen equipment. We also take care of your plants, run errands, and handle in-person check-ins and check-outs, including airport transfers. Monthly services include vacuuming air conditioner filters, cleaning windows, and inspecting sheets and towels for damage or wear.",
@@ -42,7 +42,7 @@ export const EXPERIENCE: ExperienceProp[] = [
   {
     dates: "Preventative Maintenance",
     title: "Safeguard Your Property:",
-    company: " for Longevity and Guest Satisfaction",
+    company: "$70 per hour",
     companyUrl: "https://rpm-sxm.s3.us-east-2.amazonaws.com/Preventative.pdf",
     description:
       "Preventative maintenance is crucial for extending the life of your property’s assets and avoiding costly repairs. After each rental season, we conduct a deep cleaning of the entire apartment, focusing on high-use areas like mattresses, couches, air conditioning ducts, and drains. These measures help to maintain the property’s cleanliness and functionality, ensuring it is always ready for the next guest.",
@@ -60,7 +60,7 @@ export const EXPERIENCE: ExperienceProp[] = [
   {
     dates: "Breakdowns and Repairs",
     title: "Swift Repairs & Upgrades: ",
-    company: " Keeping Your Property in Top Shape",
+    company: "$80 per hour",
     companyUrl: "https://rpm-sxm.s3.us-east-2.amazonaws.com/Breakdown.pdf",
     description:
       "Despite the best maintenance efforts, breakdowns can happen. Royal Property Management is prepared to address any issues that arise promptly and effectively. Whether it’s a malfunctioning appliance, plumbing problem, or electrical issue, our team is on hand to provide swift repairs, minimizing downtime and ensuring your property remains in excellent condition. We also offer revamp and upgrade services, from bedding and paint to furniture and kitchen items, to keep your property modern and appealing.",
